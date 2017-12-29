ee0539a33c485430b7171c7250ff8c52012240bf
wen-sf <whx_ios1@163.com>
2017-12-29 15:19:54 +0800

    创建项目

---
+++ PravicyPhoto/PravicyPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PravicyPhoto
+//
+//  Created by HongXiangWen on 2017/12/29.
+//  Copyright © 2017年 WHX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
